0fa63b8a62a7496b8603bde77ceeed2d178d9fad
조현일 <himzei@gmail.com>
2022-11-18 07:16:06 +0900

    graphql setup

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
+
+const server = new ApolloServer({
+  schema,
+});
+
+const PORT = process.env.PORT;
+
+server
+  .listen(PORT)
+  .then(() =>
+    console.log(`😄✅ Server is running on http://localhost:${PORT}/`)
+  );
